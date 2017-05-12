2e38914a2e5820923df1dc918de5cb72961faba6
dorongnyong <dorongnyong.it@gmail.com>
2017-05-12 03:21:19 -0700

    branch1 생성

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
 var http = require('http');
+var fs = require('fs');
 
-http.createServer(function (req, res) { 
-	res.writeHead(200, {'Content-Type': 'text/plain'}); 
-	res.end('Hello World\n'); 
+http.createServer(function (req, res) {
+	fs.readFile('index.html','utf8',function(err,data){
+		if(err){
+			throw err;
+		} else{
+			res.writeHead(200, {'Content-Type': 'text/plain'});
+		        res.end(data);
+		}	
+	}); 
 }).listen(3000, "127.0.0.1");
 
 console.log('Server running at http://127.0.0.1:3000/');
